eb71991d01e3d2134a4985ffeffe304cf174e4cf
dariusnew1 <96852640+dariusnew1@users.noreply.github.com>
2021-12-30 10:37:07 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var box
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+if(keyDown("right")){
+box.position.x=box.position.x+5
+}
+if(keyDown("left")){
+box.velocityX=-5
+}
+}
+
+
+
+
